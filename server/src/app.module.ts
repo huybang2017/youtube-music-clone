90a8d4e74d9808c827279ddb07d35177dca95a20
huybang2017 <ndhbang2017@gmail.com>
2025-05-25 01:54:24 +0700

    chore: update dependences

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,36 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from 'src/prisma.service';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { PrismaModule } from 'src/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
+import { SongModule } from './song/song.module';
+import { CloudinaryModule } from './cloudinary/cloudinary.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+      }),
+    }),
+    PrismaModule,
+    AuthModule,
+    UserModule,
+    SongModule,
+    CloudinaryModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
+  exports: [PrismaService],
 })
 export class AppModule {}
