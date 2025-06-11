8614a3544b3e0454bbfd0eedb0ae8f65664c5049
huybang2017 <ndhbang2017@gmail.com>
2025-06-11 13:16:03 +0700

    feat: add api get genres

--- server/src/genre/genre.controller.ts
+++ server/src/genre/genre.controller.ts
@@ -1,6 +1,14 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { GenreService } from 'src/genre/genre.service';
 
 @ApiTags('Genres')
 @Controller('genres')
-export class GenreController {}
+export class GenreController {
+  constructor(private readonly genreService: GenreService) {}
+  @Get()
+  @ApiOperation({ summary: 'Lấy tất cả thể loại' })
+  findAll() {
+    return this.genreService.findAll();
+  }
+}
