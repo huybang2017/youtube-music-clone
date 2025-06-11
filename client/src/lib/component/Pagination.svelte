3b4ec2c9c2550bb740eeef7d4fab040359bfd4b5
huybang2017 <ndhbang2017@gmail.com>
2025-06-11 13:13:34 +0700

    feat: add ui pagination

---
+++ client/src/lib/component/Pagination.svelte
@@ -0,0 +1,32 @@
+<div class="flex items-center justify-between gap-4 px-4 py-2">
+	<button
+		class="border-1 rounded-4xl cursor-pointer border-neutral-400 px-4 py-2 text-sm font-semibold transition-colors duration-300 hover:bg-neutral-400 dark:border-neutral-700 dark:hover:bg-neutral-700"
+		>Xem thêm
+	</button>
+	<button
+		class="border-1 flex cursor-pointer items-center justify-center rounded-full border-neutral-400 p-2 transition-colors duration-300 hover:bg-neutral-400 dark:border-neutral-700 dark:hover:bg-neutral-700"
+		aria-label="Back"
+	>
+		<svg
+			viewBox="0 0 24 24"
+			fill="currentColor"
+			xmlns="http://www.w3.org/2000/svg"
+			class="h-5 w-5 text-black dark:text-neutral-200"
+		>
+			<path d="M14.6,18.4L8.3,12l6.4-6.4l0.7,0.7L9.7,12l5.6,5.6L14.6,18.4z" />
+		</svg>
+	</button>
+	<button
+		class="border-1 flex cursor-pointer items-center justify-center rounded-full border-neutral-400 p-2 transition-colors duration-300 hover:bg-neutral-400 dark:border-neutral-700 dark:hover:bg-neutral-700"
+		aria-label="Next"
+	>
+		<svg
+			viewBox="0 0 24 24"
+			fill="currentColor"
+			xmlns="http://www.w3.org/2000/svg"
+			class="h-5 w-5 text-black dark:text-neutral-200"
+		>
+			<path d="M9.4,18.4l-0.7-0.7l5.6-5.6L8.6,6.4l0.7-0.7l6.4,6.4L9.4,18.4z" />
+		</svg>
+	</button>
+</div>
