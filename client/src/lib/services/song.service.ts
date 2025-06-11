b7dbdf45249817289335013b90d46ec3d23a044c
huybang2017 <ndhbang2017@gmail.com>
2025-06-11 13:16:58 +0700

    chore: add header config

--- client/src/lib/services/song.service.ts
+++ client/src/lib/services/song.service.ts
@@ -1,13 +1,17 @@
 import axiosInstance from '$lib/services/axios';
+import type { RawAxiosRequestHeaders } from 'axios';
 
-const fetchSongs = async () => {
+const fetchSongs = async (headers: RawAxiosRequestHeaders = {}) => {
 	try {
-		const response = await axiosInstance.get('/song');
+		const response = await axiosInstance.get('/song', {
+			headers
+		});
 		return response.data;
 	} catch (error) {
 		console.error('Error fetching songs:', error);
 		throw error;
 	}
 };
+
 const songService = { fetchSongs };
 export default songService;
