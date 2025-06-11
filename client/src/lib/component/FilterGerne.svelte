75ddac1f0a0579c7fe66b6df2c775c939901b53d
huybang2017 <ndhbang2017@gmail.com>
2025-06-11 13:13:43 +0700

    feat: add ui filter gerne

---
+++ client/src/lib/component/FilterGerne.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let genre: {
+		id: string;
+		name: string;
+	};
+</script>
+
+<div class="flex items-center gap-3">
+	<button
+		class="transform cursor-pointer rounded-lg border-0 bg-neutral-200 p-2 text-center
+			text-sm text-black
+			transition-transform
+			hover:bg-neutral-300 dark:bg-neutral-800
+			dark:text-white
+			hover:dark:bg-neutral-600"
+	>
+		<h3 class="whitespace-nowrap">{genre.name}</h3>
+	</button>
+</div>
